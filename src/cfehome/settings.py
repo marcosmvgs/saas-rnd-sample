b7385a9cb62a77d47036280c8fca7cef4b018b8f
marcosmvgs <13080marcos.tupa@gmail.com>
2024-07-23 19:00:52 -0300

    Preparing for Railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e0%$e&1#h+c59#jac3y6ehku(gtg_8v3tov=+p7xde%9=^!-c$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True # os.environ.get("DEBUG") or False
+print("DEBUG", DEBUG)
+
 
 ALLOWED_HOSTS = [
   ".railway.app" 
